3949676c7706b020306972cd4c5e98a08f98aef6
careylzh <lzh.carey@gmail.com>
2026-05-29 22:50:48 +0800

    Initial Commit

---
+++ mac2visionOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mac2visionOS
+//
+//  Created by Carey Lai  on 29/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mac2visionOS/mac2visionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac2visionOSApp.swift
+//  mac2visionOS
+//
+//  Created by Carey Lai  on 29/5/26.
+//
+
+import SwiftUI
+
+@main
+struct mac2visionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
